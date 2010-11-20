7d8d2a6b112c0862722edacafd1ab5f7ce4181f2
Toshiyuki Hirooka <toshi.hirooka@gmail.com>
2010-11-20 16:54:23 +0900

    add monkey patch for rcov

--- test/helper.rb
+++ test/helper.rb
@@ -17,3 +17,19 @@ require 'automaze'
 
 class Test::Unit::TestCase
 end
+
+# monkey patch for rcov encoding problem
+# https://github.com/relevance/rcov/issues/labels/fix#issue/31
+if defined? Rcov
+  module Rcov
+    class CodeCoverageAnalyzer < DifferentialAnalyzer
+      def update_script_lines__
+        if '1.9'.respond_to?(:force_encoding)
+          SCRIPT_LINES__.each{|k,v| v.each{|src| src.force_encoding('utf-8')}}
+        end
+
+        @script_lines__ = @script_lines__.merge(SCRIPT_LINES__)
+      end
+    end
+  end
+end
