375475cb65da1453a9d00a35f6e4fc1de94bb737
Toshiyuki Hirooka <tosik@mactosik.local>
2011-02-13 04:09:13 +0900

    using Benchmark lib on example

--- examples/benchmark.rb
+++ examples/benchmark.rb
@@ -1,14 +1,14 @@
+require 'benchmark'
+
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
-
 require "automaze"
 
-started = Time.now
-maze = Automaze::Automaze.new(
-  :algorithm=>:dug_tunnels,
-  :size_x=>50,
-  :size_y=>50)
-
-puts "#{Time.now - started} seconds"
-
+puts Benchmark::CAPTION
+puts Benchmark.measure {
+  Automaze::Automaze.new(
+    :algorithm=>:dug_tunnels,
+    :size_x=>50,
+    :size_y=>50)
+}
 
